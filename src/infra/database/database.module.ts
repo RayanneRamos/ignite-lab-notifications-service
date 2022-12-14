7e084e9979c7725a3ef2dd6426909ce141bf1003
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-12-14 19:59:09 -0300

    Colocando em pastas os arquivos

---
+++ src/infra/database/database.module.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import { Module } from "@nestjs/common";
+import { NotificationsRepository } from "src/application/repositories/notification-repository";
+import { PrismaService } from "./prisma/prisma.service";
+import { PrismaNotificationRepository } from "./prisma/repositories/prisma-notifications-repositories";
+
+@Module({
+  providers: [PrismaService, {
+    provide: NotificationsRepository,
+    useClass: PrismaNotificationRepository,
+  }],
+  exports: [
+    NotificationsRepository,
+  ],
+})
+
+export class DatabaseModule {}
