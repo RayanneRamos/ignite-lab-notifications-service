dfc22bb8560dda4ad9a2a61fa70b9d2db25ce8f5
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-12-15 15:10:40 -0300

    Criando o read-notification e seus testes

--- src/application/use-cases/read-notification.ts
+++ src/application/use-cases/read-notification.ts
@@ -3,19 +3,19 @@ import { Injectable } from '@nestjs/common';
 import { NotificationsRepository } from '../repositories/notification-repository';
 import { NotificationNotFound } from './errors/notification-not-found';
 
-interface CancelNotificationRequest {
+interface ReadNotificationRequest {
   notificationId: string;
 }
 
-type CancelNotificationResponse = void;
+type ReadNotificationResponse = void;
 
 @Injectable()
-export class CancelNotification {
+export class ReadNotification {
   constructor(private notificationRepository: NotificationsRepository) {}
   
   async execute(
-    request: CancelNotificationRequest,
-  ): Promise<CancelNotificationResponse> {
+    request: ReadNotificationRequest,
+  ): Promise<ReadNotificationResponse> {
     const { notificationId } = request;
 
     const notification = await this.notificationRepository.findById(notificationId);
@@ -24,7 +24,7 @@ export class CancelNotification {
       throw new NotificationNotFound();
     }
 
-    notification.cancel();
+    notification.read();
 
     await this.notificationRepository.save(notification);
   }
