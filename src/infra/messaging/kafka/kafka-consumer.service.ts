be9210137fd54ec3294b8dcdc31bc5dde2371bba
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-12-17 12:34:23 -0300

    Mudando as variáveis de ambiente

--- src/infra/messaging/kafka/kafka-consumer.service.ts
+++ src/infra/messaging/kafka/kafka-consumer.service.ts
@@ -14,9 +14,8 @@ export class KafkaConsumerService
         brokers: ['useful-parrot-11520-us1-kafka.upstash.io:9092'],
         sasl: {
           mechanism: 'scram-sha-256',
-          username:
-            'dXNlZnVsLXBhcnJvdC0xMTUyMCRCg4suxcjTuY8nlkgNu78co5T-g1bmiLCtC58',
-          password: 'lT4ofly1rpKKTYfAFq7EZx_MJIwbljCIZDCX5Q4CRMVLMBCqB2nNNubG7vN6u2Ya7yY1HQ==',
+          username:'{{ UPSTASH_KAFKA_USERNAME }}',
+          password: '{{ UPSTASH_KAFKA_PASSWORD }}',
         },
         ssl: true,
       },
